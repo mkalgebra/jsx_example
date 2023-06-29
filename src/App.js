4299e8e375923a0084faf73820f7b0525b5ed5e1
Marijo Kis <marijo.kis@q.agency>
2023-06-29 20:06:43 +0200

    chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const dog = 'Pesek';
+
+  const cat = {
+    name: 'Silvestar',
+    color: 'White',
+    age: '2'
+  }
+
+  function kittySaysHi(maca){
+    return `Ime moje mace je ${maca.name}. Ona je ${maca.color}. Ima ${cat.age} godina.`
+  }
+
+  const food = ['pizza', 'burger', 'lasagne']
+
+  const shoot = 2
+
+  function goal(sh){
+    // if(sh%2===0){
+    //   return 'Gooooooool'
+    // }else{
+    //   return 'Stativa'
+    // }
+    return sh%2===0 ? 'Goooool' : 'Stativa'
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Svasta nesta</h1>
+      <span>Vau vau: {dog}</span>
+      <p>Ime moje mace je {cat.name}. Ona je {cat.color} boje. Ima {cat.age} godina.</p>
+      <p>{kittySaysHi(cat)}</p>
+      <p>Moja omiljena hrana je: {food.join(',')}</p>
+      <p>Iiiii prilikaaaaa..... {goal(shoot)}</p>
     </div>
   );
 }
